1dc0ae1c2c2e9d95804ccb8f900f958648e2b388
shannon-nabors <shannon.nabors@gmail.com>
2019-01-15 13:56:20 -0500

    Done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,25 @@
 class BoatingTest
 
+  attr_reader :student, :test_name, :instructor
+  attr_accessor :status
+  @@all = []
+
+  def initialize(student, test_name, status, instructor)
+    @student = student
+    @test_name = test_name
+    @status = status
+    @instructor = instructor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.test_exists?(student, test_name)
+    self.all.find do |test|
+      test.test_name == test_name && test.student == student
+    end
+  end
+
 end
