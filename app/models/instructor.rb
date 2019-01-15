598b3db929352cc59a02a8a96ba076103daf442c
dcjones1 <dcejjones@gmail.com>
2019-01-15 15:48:46 -0500

    revised

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -21,8 +21,8 @@ class Instructor
   end
 
   def fail_student(student ,test_name)
-    if BoatingTest.all.find_index {|test| test.test_name == test_name}
-      index = BoatingTest.all.find_index {|test| test.test_name == test_name}
+    if BoatingTest.all.find_index {|test| test.student == student && test.test_name == test_name && test.instructor == self}
+      index = BoatingTest.all.find_index {|test| test.student == student && test.test_name == test_name && test.instructor == self}
       BoatingTest.all[index].test_status = "failed"
     else
       self.create_test(student, test_name, "failed")
