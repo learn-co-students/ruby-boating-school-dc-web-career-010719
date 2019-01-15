b8ae498c36c5f046e2710642867b37b20037599a
Anthony Gregg <agregg22@gmail.com>
2019-01-14 21:03:07 -0500

    Complete

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,53 @@
+require "pry"
+
 class Instructor
 
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student_name, test_name)
+    student_pass = Student.find_student(student_name)
+     specific_test = BoatingTest.all.find { |test| test.student == student_pass && test.name == test_name && test.instructor == self }
+    if specific_test.nil?
+      BoatingTest.new(student_pass, test_name, "passed", self)
+    else
+      specific_test.status = "passed"
+    end
+    specific_test
+  end
+
+  def fail_student(student_name, test_name)
+    student_fail = Student.find_student(student_name)
+     specific_test = BoatingTest.all.find { |test| test.student == student_fail && test.name == test_name && test.instructor == self }
+     # binding.pry
+    if specific_test.nil?
+      BoatingTest.new(student_fail, test_name, "failed", self)
+    else
+      specific_test.status = "failed"
+    end
+    specific_test
+  end
+
 end
+
+  # def fail_student(student_name, test_name)
+  #   student_fail = Student.find_student(student_name)
+  #     BoatingTest.all.find { |test| test.student == student_fail && test.name == test_name && test.instructor == self }
+  #         test.status = "failed"
+  #     else
+  #       # binding.pry
+  #       BoatingTest.new(student_fail, test_name, "failed", self)
+  #     end
+  #   end
+
+# end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,25 @@
 require 'bundler/setup'
 Bundler.require
 require_all 'app'
+
+
+spongebob = Student.new("Spongebob")
+patrick = Student.new("Patrick")
+
+puff = Instructor.new("Ms.Puff")
+krabs = Instructor.new("Mr.Krabs")
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+fine = spongebob.add_boating_test("Don't Crash 202", "passed", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "pending", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 204", "passed", krabs)
+
+puts spongebob.name == "Spongebob"
+puts patrick.name == "Patrick"
+puts puff.name == "Ms.Puff"
+puts krabs.name == "Mr.Krabs"
+# binding.pry
+puts no_crashing.status == "pending"
+puts Student.find_student("Spongebob") == spongebob
+puts spongebob.grade_percentage
+puts puff.pass_student("Spongebob", "Don't Crash 105")
+puts puff.fail_student("Patrick" , "Power Steering 202")
