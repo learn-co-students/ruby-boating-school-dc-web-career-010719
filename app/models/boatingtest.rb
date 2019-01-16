41197f52741034d81114b88a0c9aa4ee4b82f8ba
mkay0961 <mkay0961@gmail.com>
2019-01-16 14:57:08 -0500

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,16 @@
 class BoatingTest
 
+  attr_accessor :student, :name, :status, :instructor
+
+  @@all = []
+
+  def initialize(student, name, status, instructor)
+    @student, @name, @status, @instructor = student, name, status, instructor
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
 end
